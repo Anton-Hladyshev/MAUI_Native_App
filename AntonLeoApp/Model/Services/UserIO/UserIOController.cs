e09e8d1a0d05d2f6ca10c45994d4561759126bda
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2026-03-24 17:52:25 +0100

    add GetAllCharacters to access all characters from the local JSON file

--- AntonLeoApp/Model/Services/UserIO/UserIOController.cs
+++ AntonLeoApp/Model/Services/UserIO/UserIOController.cs
@@ -1,9 +1,34 @@
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
 namespace AntonLeoApp.Model.Services.UserIO;
 
 public class UserIOController
 {
-    private const string _dataPath = "../../Data/local.json";
+    private readonly string _dataPath = Path.Combine(FileSystem.AppDataDirectory, "local.json");
     private const string _mediaPath = "../../Data/media/";
 
-    
+    private readonly JsonSerializerOptions _options = new()
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    };
+
+    public async Task<List<UserCharacter>> GetAllCaharacters()
+    {
+        if (!File.Exists(_dataPath))
+        {
+            return new List<UserCharacter>();
+        }
+
+        try
+        {
+            string json = await File.ReadAllTextAsync(_dataPath);
+            return JsonSerializer.Deserialize<List<UserCharacter>>(json) ?? new List<UserCharacter>();
+        }
+        catch
+        {
+            return new List<UserCharacter>();
+        }
+    }
 }
