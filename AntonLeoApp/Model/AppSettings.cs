b0932cebb3ed447d6ecf745a68c9f60580070aab
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2026-03-24 17:13:38 +0100

    add the Model structure to add the user input/output support

--- AntonLeoApp/Model/AppSettings.cs
+++ AntonLeoApp/Model/AppSettings.cs
@@ -3,4 +3,5 @@ namespace AntonLeoApp.Model;
 public static class AppSettings
 {
     public const string API_DEFAULT_URL = "https://starwars-databank-server.onrender.com/api/v1/";
+    public const string API_CARDS_DEFAULT_URL = "https://api.swu-db.com/cards/sor";
 }
---
+++ AntonLeoApp/Model/Services/UserIO/UserIOController.cs
@@ -0,0 +1,9 @@
+namespace AntonLeoApp.Model.Services.UserIO;
+
+public class UserIOController
+{
+    private const string _dataPath = "../../Data/local.json";
+    private const string _mediaPath = "../../Data/media/";
+
+    
+}
